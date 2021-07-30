60e354055119b96909584132c84bf2bce61ad59d
Manav Gopal <gopal.1@iitj.ac.in>
2021-07-30 23:09:14 +0530

    added comments to understand

--- enron.py
+++ enron.py
@@ -1,55 +1,62 @@
-import pyttsx3
-#text-to-speeach converter library
-import datetime
-import speech_recognition as sr
-import wikipedia
-import webbrowser
-import os
-import smtplib
-import random
- 
-engine = pyttsx3.init("sapi5")
-voices = engine.getProperty("voices")
+import pyttsx3  # Text-to-speeach converter library
+import datetime  # Libraries to deal with date and time
+import speech_recognition as sr # Library for performing speech recognition https://pypi.org/project/SpeechRecognition/
+import wikipedia    # In order to extract data from Wikipedia,  Python Wikipedia library is used, which wraps the official Wikipedia API
+import webbrowser   # The webbrowser module can be used to launch a browser in a platform-independent manner
+import os        # The OS module in Python provides functions for creating and removing a directory (folder), fetching its contents, changing and identifying the current directory, etc.
+import smtplib  # The smtplib module defines an SMTP client session object that can be used to send mail to any Internet machine with an SMTP or ESMTP listener daemon
+import random   # For generating any random number
+
+# The below code is to set the webBrowser
+# If this piece of code is deleted then the program will automatically open in default web browser
+webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+webbrowser = webbrowser.get('chrome')
+
+engine = pyttsx3.init("sapi5") # The pyttsx3 module supports two voices first is female and the second is male which is provided by “sapi5” for windows.
+# sapi5 – SAPI5 on Windows
+# nsss – NSSpeechSynthesizer on Mac OS X
+# espeak – eSpeak on every other platform
+voices = engine.getProperty("voices")   # Get all the voices from the system
 # print(voices[1].id)
-engine.setProperty("voice",voices[0].id)
+engine.setProperty("voice",voices[0].id)    # Set a particular voice for the engine
 
-
+# Define a function which will call the engine to speak the parameter passed and then run and wait
 def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
 
+    engine.say(audio)   
+    # The runAndWait Gets if the engine is currently busy speaking an utterance or not. ... True if speaking, false if not. runAndWait () → None. Blocks while processing all currently queued commands.
+    engine.runAndWait() # This function will make the speech audible in the system, if you don't write this command then the speech will not be audible to you.
+
+# This function greets when the program is run
 def wishMe():
-    '''
-    it greets when the program is run
 
-    '''
-    hour = int(datetime.datetime.now().hour)
+    hour = int(datetime.datetime.now().hour)  # Getting the hour from the date time library
     if hour>=12 and hour<18:
-        speak("Good Afternoon, sir")
+        speak("Good Afternoon, sir")    
     elif hour<12 and hour>=0:
         speak("Good Morning, sir")
     else:
         speak("Good Evening, sir")
 
-    speak("I am brainiac. Please tell me how may i help you.")
+    speak("I am brainiac. How may i help you.")
 
+# This function takes microphone input from the user and returns string output
 def takeCommand():
-    '''
-    it takes microphone input from the user and returns string output
-
-    '''
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source,duration=1)
+    
+    r = sr.Recognizer()  # This Speech recoganizer enables for a machine or program to identify words spoken aloud and convert them into readable text.
+    with sr.Microphone() as source:  #pyAudio needs to be installed
+        r.adjust_for_ambient_noise(source,duration=1) # This func is defined under the class Recoganiser of the speech recognisition
+        # The ``duration`` parameter is the maximum number of seconds that it will dynamically adjust the threshold for before returning.
         print("Listening...")
-        r.pause_threshold = 1
+        r.pause_threshold = 1   # seconds of non-speaking audio before a phrase is considered complete
         # r.energy_threshold = 500  this is if there is more noise in surrounding then you dont want them to be listened.
-        audio = r.listen(source)
+        audio = r.listen(source)    ## This func is defined under the class Recoganiser of the speech recognisition
 
     try:
         print("Recoganizing...")
-        query = r.recognize_google(audio,language="en-in")
-        print(f"User said: {query}\n")
+        # Here query is a string
+        query = r.recognize_google(audio,language="en-in")  # Performs speech recognition on ``audio_data`` (an ``AudioData`` instance), using the Google Speech Recognition API.
+        print(f"User said: {query}\n")  # This will print the query said by the user
         # speak(query)
     except Exception as e:
         # print(e)
@@ -59,10 +66,18 @@ def takeCommand():
 
 def sendEmail(to, content):
     # first allow less secure app in your gmail account from which you are sending mail
+    
     server = smtplib.SMTP("smtp.gmail.com",587)
-    server.ehlo()
+    server.ehlo()  
+    # Extended HELO (EHLO) is an Extended Simple Mail Transfer Protocol (ESMTP) command sent by an email server to identify itself when connecting to another email server to start the process of sending an email
+
     server.starttls()
+    # STARTTLS is an email protocol command that tells an email server that an email client, including an email client running in a web browser, wants to turn an existing insecure connection into a secure one.
+
+    # It will login to the email given
     server.login("manavsharma1208@gmail.com","Your-password")
+
+    # here is the email of the sender
     server.sendmail("manavsharma1208@gmail.com",to,content)
     server.close()
     
@@ -70,8 +85,9 @@ if __name__ == "__main__":
     wishMe()
     while True:
         
-        query = takeCommand().lower() #takecommand will covenrt the speaked sentence into string
+        query = takeCommand().lower() # Takecommand will covenrt the speaked sentence into string
         #logic for executing tasks based on query
+
         if "hello" in query:
             speak("hi sir")
         elif "wikipedia" in query:
@@ -84,7 +100,7 @@ if __name__ == "__main__":
 
         elif "open youtube" in query:
             speak("sure sir, Opening youtube")
-            webbrowser.open("youtube.com")
+            webbrowser.open("youtube.com") # This will open youtube using the webbrowser library in the browser specified
 
         elif "open google" in query:
             speak("opening google")
@@ -104,25 +120,25 @@ if __name__ == "__main__":
 
         elif "play music" in query:
             speak("here is the song for you...sir")
-            music_dir="C:\\Users\\manav\\OneDrive\\Desktop\\my music"
-            songs = os.listdir(music_dir)
+            music_dir="C:\\Users\\manav\\OneDrive\\Desktop\\my music"  #Specifing the directory address in which musics are kept
+            songs = os.listdir(music_dir)    # This command will list all the music from the music directory provided
             #Further Use a loop to generate a random song
             # print(songs)
-            rand_song=random.randint(0,len(songs)-1)
-            os.startfile(os.path.join(music_dir,songs[rand_song]))
+            rand_song=random.randint(0,len(songs)-1)    # This willcreate a random songs number from all the songs
+            os.startfile(os.path.join(music_dir,songs[rand_song]))     # This command will first join the path of music dir to the path od os and then start that file 
 
         elif "the time" in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"Sir, the time is {strTime}")
+            strTime = datetime.datetime.now().strftime("%H:%M:%S") # This will fetch the current time from datetime library
+            speak(f"Sir, the time is {strTime}")    # It will speak the current time
 
         elif "open code" in query:
-            codePath = "E:\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
+            codePath = "E:\\Microsoft VS Code\\Code.exe"       # Specifying the path of vscode
+            os.startfile(codePath)  
         elif "email to manav" in query:
             #make a dictonary with key=name and value=emailid
             try:
                 speak("what should i say")
-                content = takeCommand()
+                content = takeCommand()     # It will take the spoke command by user and return an string of that using google sppech_recognition library
                 to = "gopal.1@iitj.ac.in"
                 sendEmail(to, content)
                 speak("Email has been sent!")
